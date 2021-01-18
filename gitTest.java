888c362e5f7a6760ff49c42bccc462b4e4982fee
rql1986 <rql1986@hotmail.com>
2021-01-19 00:29:28 +0800

    修改完成，增加类名

--- gitTest.java
+++ gitTest.java
@@ -1,3 +1,6 @@
-public static void main(String[] args){
-    System.out.println("hello git");
+
+public class gitTest {
+    public static void main(String[] args){
+        System.out.println("hello git");
+    }
 }
